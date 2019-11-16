5d9d47ab223fb267945c7c27a1359229635d45da
CICI <43932741+SXU-CICI@users.noreply.github.com>
2019-11-16 11:27:07 +0800

    Update genhtml.py

--- genhtml.py
+++ genhtml.py
@@ -1,8 +1,9 @@
 import json
-import fire
+# import fire
+import jieba
 class GenHtml(object):
     def __init__(self):
-        self.json_path = 'attn_data.json'
+        self.json_path = 'result/attn_data.json'
 
     def load_json(self):
         with open(self.json_path, 'r', encoding='utf-8') as f:
@@ -13,10 +14,11 @@ class GenHtml(object):
         html_color = '#%02X%02X%02X' % (255, int(255 * (1 - attn)), int(255 * (1 - attn)))
         return '<span style="background-color: {}">{}</span>'.format(html_color, word)
 
-    def mk_html(self, seq, attns):
+    def mk_html(self, seqs_token, attns):
         html = ""
-        attns = attns[0:len(seq)]
-        for ix, attn in zip(seq, attns):
+        if attns == 1:
+            attns = [1]
+        for ix, attn in zip(seqs_token, attns):
             html += ' ' + self.highlight(ix, attn)
         return '<li>' + html + '</li>'
 
@@ -26,7 +28,7 @@ class GenHtml(object):
         html = html + '<span>' + "预测标签：" + str(pre_label) + '</span>'
         return '<li>' + html + '</li>'
 
-    def gen_html(self):
+    def gen_html(self,args):
         seqs = self.json_data['sequences']
         attns = self.json_data['attention_weights']
         label = self.json_data['rea_labels']
@@ -35,13 +37,18 @@ class GenHtml(object):
         text = '<ul>'
         for i in range(batch_size):
             text += self.mk_label(label[i],pre_label[i])
-            for j in range(3):
-                text += self.mk_html(seqs[i], attns[i*3+j])
+            if args.attention_layer == 'mpoa' or  args.attention_layer == 'mpa':
+                for j in range(args.num_classes):
+                    seq_tokens = jieba.lcut(seqs[i][0])
+                    text += self.mk_html(seq_tokens, attns[i*3+j])
+            else:
+                seq_tokens = jieba.lcut(seqs[i][0])
+                text += self.mk_html(seq_tokens, attns[i])
         text += '</ul>'
         self.text = text
 
-    def save(self,weight_type,save_path):
-        with open(save_path+weight_type+'.html', 'w', encoding='utf-8') as f:
+    def save(self,save_path):
+        with open(save_path, 'w', encoding='utf-8') as f:
             head = '''
             <!DOCTYPE html>
             <html lang="en">
@@ -58,20 +65,13 @@ class GenHtml(object):
             self.text = head + self.text + tail
             f.write(self.text)
 
-    def gen(self, attn_data, weight_type, save_path):
+    def gen(self, attn_data,save_path,args):
         self.json_data = attn_data
-        self.gen_html()
-        self.save(weight_type,save_path)
+        self.gen_html(args)
+        self.save(save_path)
 
 if __name__ == '__main__':
     fire.Fire(GenHtml)
-
-
-
-
-
-
-
-
-
-
+    genHtml = GenHtml()
+    attn_data = genHtml.load_json()
+    genHtml.gen(attn_data, 'result/attention.html')
